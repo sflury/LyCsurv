d246eb71492afda5b7cf047b53ec0d16de6d3b2f
Sophia Flury <42982705+sflury@users.noreply.github.com>
2024-01-15 10:22:22 -0500

    Update LyCsurv.py

--- LyCsurv.py
+++ LyCsurv.py
@@ -2,25 +2,26 @@ import numpy as np
 import pandas as pd
 import sys
 from lifelines import CoxPHFitter,WeibullAFTFitter
-'''
-Name:
-    interp_ph
 
-Purpose:
-    Interpolate over the survival function for each input observation to predict
-    the appropriate escape fraction.
+def InterpPH(dat,part,base):
+    '''
+    Name:
+        InterpPH
 
-Arugments:
-    :dat (*pandas.DataFrame*): pandas DataFrame of observables used to train model
-    :part (**): Cox partial proportional hazard values for survival function
-    :base (**): predicted response corresponding to full range of observations
+    Purpose:
+        Interpolate over the survival function for each input observation to predict
+        the appropriate escape fraction.
 
-Returns:
-    :predict (*np.ndarray*): Nx3 array of response values predicted by the Cox PH
-            model. First row is the lower uncertainty. Middle row is the median.
-            Last row is the upper uncertainty.
-'''
-def interp_ph(dat,part,base):
+    Arugments:
+        :dat (*pandas.DataFrame*): pandas DataFrame of observables used to train model
+        :part (**): Cox partial proportional hazard values for survival function
+        :base (**): predicted response corresponding to full range of observations
+
+    Returns:
+        :predict (*np.ndarray*): Nx3 array of response values predicted by the Cox PH
+                model. First row is the lower uncertainty. Middle row is the median.
+                Last row is the upper uncertainty.
+    '''
     # storage array of predicted values
     f = np.zeros((len(dat),4))
     # for each object
@@ -36,32 +37,81 @@ def interp_ph(dat,part,base):
     # convert percentiles into uncertainties
     f[:,[0,2]] = abs(np.diff(f[:,:3],axis=1))
     return f
-'''
-Name:
-    cox_ph
 
-Purpose:
-    Perform a Cox proportional hazards regression on the input reference data
+def ModAssess(trn,mod,cens,concord='harrell'):
+    '''
+    Name:
+        ModAssess
 
-Arguments:
-    :dat (*pandas.DataFrame*): pandas DataFrame containing columns named
-            according to conventions in `params.lis` with values corresponding
-            to the galaxy sample which the user desires to fit
+    Purpose:
+        Assess the quality of a survival model by testing it against the training
+        data set (in this case, the LzLCS).
 
-Keyword Arguments:
-    :resp (*str*): string indicating the desired response variable. Options are
-            'f_esc(LyC)', 'f_esc(LyA)', 'f(LyC)', and 'f(LyA)'. Default is
-            'f_esc(LyC)'.
-    :verbose (*bool*): boolean indicating whether to print out details of the
-            Cox proportional hazards regression. Default is `False`.
+    Arguments:
+        :trn (*np.ndarray*): Nx1 array of observed values
+        :mod (*np.ndarray*): Nx1 array of predicted values
+        :cens (*np.ndarray*): Nx1 array of censors as booleans
 
-Returns:
-    :cph_fit (*numpy.ndarray*): n by 4 array containing the median, lower and
-            upper uncertainties corresponding to 0.16 and 0.84 quantiles, and
-            an indicator of whether the survival function is always below (-1)
-            or above (+1) the median of the predicted distribution
-'''
-def cox_ph(dat,resp='f_esc(LyC)',verbose=False):
+    Keyword Arguments:
+        :concord (*str*): string indicating the method to use for concordance
+            calculation. Currently supports Harrell+ 1996 and Uno+ 2011 methods.
+            Default is \'harrell\'.
+
+    Returns:
+        :R2  (*float*): R^2 metric of the residuals
+        :R2a (*float*): adjusted R^2 metric of the residuals
+        :RMS (*float*): root-mean-square of the residuals
+        :C   (*float*): concordance index
+    '''
+    trnLog = np.log10(trn)
+    modLog = np.log10(mod)
+    n = len(trn)
+    R2   = 1 - np.sum(np.square(trnLog-modLog)) / \
+               np.sum(np.square(trnLog-trnLog.mean()))
+    R2a  = 1 - (1-R2)*(n-1)/(n-p-1)
+    RMS  = np.sqrt(np.sum(np.square(trnLog-modLog))/n)
+    if concord.lower() == 'harrell':
+        from sksurv.metrics import concordance_index_censored
+        C = concordance_index_censored(cens,trnLog,modLog)[0]
+    elif concord.lower() == 'uno':
+        from sksurv.metrics import concordance_index_ipcw
+        C = concordance_index_ipcw(trnLog,modLog)[0]
+    #elif concord.lower() == 'auc':
+    #    from sksurv.metrics import cumulative_dynamic_auc
+    return R2,R2a,RMS,C
+
+def CoxPH(dat,resp='f_esc(LyC)',verbose=False,StatsVerbose=False):
+    '''
+    Name:
+        CoxPH
+
+    Purpose:
+        Perform a Cox proportional hazards regression on the input reference data
+
+    Arguments:
+        :dat (*pandas.DataFrame*): pandas DataFrame containing columns named
+                according to conventions in `params.lis` with values corresponding
+                to the galaxy sample which the user desires to fit
+
+    Keyword Arguments:
+        :resp (*str*): string indicating the desired response variable. Options are
+                'f_esc(LyC)', 'f_esc(LyA)', 'f(LyC)', and 'f(LyA)'. Default is
+                'f_esc(LyC)'.
+        :verbose (*bool*): boolean indicating whether to print out details of the
+                Cox proportional hazards regression. Default is `False`.
+        :StatsVerbose (*bool*): boolean indicating whether to perform and return
+                statistical assessments of the model using the training set.
+                Default is `False`.
+
+    Returns:
+        :cph_fit (*numpy.ndarray*): n by 4 array containing the median, lower and
+                upper uncertainties corresponding to 0.16 and 0.84 quantiles, and
+                an indicator of whether the survival function is always below (-1)
+                or above (+1) the median of the predicted distribution
+        :ModAssess (*tuple*): (_optional_) 4x1 tuple of model assessments
+                containing the R^2, adjusted R^2, RMS, and concordance index.
+                Only returned if `StatsVerbose` set to `True`.
+    '''
     # read in data
     trn = pd.read_csv('./tab/lzlcs.csv')
     # censor array based on LyC detection
@@ -89,34 +139,45 @@ def cox_ph(dat,resp='f_esc(LyC)',verbose=False):
         print(trn[pred].mean(axis=0))
     # results, including quantiles on fesc predictions
     base = cph.baseline_cumulative_hazard_
-    return interp_ph(dat[pred],cph.predict_partial_hazard(dat[pred]),base)
-'''
-Name:
-    aft
+    if StatsVerbose:
+        mod = InterpPH(dat[pred],cph.predict_partial_hazard(dat[pred]),base)
+        return mod, ModAssess(trn[resp],mod[:,1],len(pred),trn['censors'])
+    else:
+        return InterpPH(dat[pred],cph.predict_partial_hazard(dat[pred]),base)
 
-Purpose:
-    Perform parametric survival regression using the Accelerated Failure method
-    assuming a generic Weibull distribution.
+def AFT(dat,resp='f_esc(LyC)',verbose=False,intercept=True,StatsVerbose=False):
+    '''
+    Name:
+        AFT
 
-Arguments:
-    :dat (*pandas.DataFrame*): pandas DataFrame containing columns named
-            according to conventions in `params.lis` with values corresponding
-            to the galaxy sample which the user desires to fit
+    Purpose:
+        Perform parametric survival regression using the Accelerated Failure method
+        assuming a generic Weibull distribution.
 
-Keyword Arguments:
-    :resp (*str*): string indicating the desired response variable. Options are
-            'f_esc(LyC)', 'f_esc(LyA)', 'f(LyC)', and 'f(LyA)'. Default is
-            'f_esc(LyC)'.
-    :verbose (*bool*): boolean indicating whether to print out details of the
-            accelerated failure regression. Default is `False`.
-    :intercept (*bool*): boolean indicating whether to include an intercept in
-            the parametric hazard function
+    Arguments:
+        :dat (*pandas.DataFrame*): pandas DataFrame containing columns named
+                according to conventions in `params.lis` with values corresponding
+                to the galaxy sample which the user desires to fit
 
-Returns:
-    :aft_fit (*np.ndarray*): n by 3 array containing the median, lower and
-            upper uncertainties corresponding to 0.16 and 0.84 quantiles
-'''
-def aft(dat,resp='f_esc(LyC)',verbose=False,intercept=True):
+    Keyword Arguments:
+        :resp (*str*): string indicating the desired response variable. Options are
+                'f_esc(LyC)', 'f_esc(LyA)', 'f(LyC)', and 'f(LyA)'. Default is
+                'f_esc(LyC)'.
+        :verbose (*bool*): boolean indicating whether to print out details of the
+                accelerated failure regression. Default is `False`.
+        :intercept (*bool*): boolean indicating whether to include an intercept in
+                the parametric hazard function
+        :StatsVerbose (*bool*): boolean indicating whether to perform and return
+                statistical assessments of the model using the training set.
+                Default is `False`.
+
+    Returns:
+        :aft_fit (*np.ndarray*): n by 3 array containing the median, lower and
+                upper uncertainties corresponding to 0.16 and 0.84 quantiles
+        :ModAssess (*tuple*): (_optional_) 4x1 tuple of model assessments
+                containing the R^2, adjusted R^2, RMS, and concordance index.
+                Only returned if `StatsVerbose` set to `True`.
+    '''
     if intercept:
         dat['Intercept'] = np.ones(len(dat))
     # read in data
@@ -150,4 +211,7 @@ def aft(dat,resp='f_esc(LyC)',verbose=False,intercept=True):
     f[f>1] = 1
     f[f<0] = 0
     f[:,[0,2]] = abs(np.diff(f,axis=1))
-    return f
+    if StatsVerbose:
+        return f, ModAssess(trn[resp],f[:,1],len(pred),trn['censors'])
+    else:
+        return f
